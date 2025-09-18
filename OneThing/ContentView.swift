8681b893c3b2f355954934c6163ddc926d9bbac0
Jackson Rich <jackson_rich@uri.edu>
2025-09-18 15:29:36 -0400

    Initial Commit

---
+++ OneThing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneThing
+//
+//  Created by Jackson Rich on 9/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneThing/OneThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneThingApp.swift
+//  OneThing
+//
+//  Created by Jackson Rich on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct OneThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
